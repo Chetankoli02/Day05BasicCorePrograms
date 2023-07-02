0b205bbe3556fa6a36d89c0648d953631e0ac160
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-02 12:48:16 +0530

    [Chetankoli]-Added Swap Two Number

--- BasicCore/Program.cs
+++ BasicCore/Program.cs
@@ -4,10 +4,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        FlipCoin flipCoin = new FlipCoin();
+        flipCoin.Main(args);
+
+        LeapYear leapYear = new LeapYear();
+        leapYear.Main(args);
+
+        Powerof2 powerof2 = new Powerof2();
+        powerof2.Main(args);
+
+        HarmonicNumber harmonicNumber = new HarmonicNumber();
+        harmonicNumber.FindHarmonicNumber();
+
+        Factors factors = new Factors();
+        factors.CalculateFactor();
 
         QuotientRemainder quotientRemainder = new QuotientRemainder();
         quotientRemainder.Main(args);
-        
+
+        Swapping swapping = new Swapping();
+        swapping.Main(args);
+
     }
 }
---
+++ BasicCore/Swapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class Swapping
+    {
+        public void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the First Number");
+            Console.WriteLine("Enter the Second Number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swapping");
+            Console.WriteLine("First Number: " + number1);
+            Console.WriteLine("Second Number: " + number2);
+
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Console.WriteLine("After swapping:");
+            Console.WriteLine("First number: " + number1);
+            Console.WriteLine("Second number: " + number2);
+        }
+    }
+}
