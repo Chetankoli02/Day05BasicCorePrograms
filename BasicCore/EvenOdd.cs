badda7d13c6242ca13e073b750f75ec418932f50
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-02 12:53:13 +0530

    [Chetankoli]-Added Even or Odd

---
+++ BasicCore/EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class EvenOdd
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + "is an Even Number");
+            }
+            else
+            {
+                Console.WriteLine(number + " is an Odd Number");
+            }
+        }
+    }
+}
