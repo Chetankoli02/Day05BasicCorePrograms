f97d563b86bbedd8ae607beb4b0f7129b514fe2e
“Chetankoli02” <chetankoli2016@gmail.com>
2023-07-02 11:44:48 +0530

    [Chetankoli]-Added Flip Coin

---
+++ BasicCore/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore
+{
+    internal class FlipCoin
+    {
+        public void Main(string[] args)
+        {
+            int tailcount = 0, headcount = 0, perOfHead, perOfTail;
+            Console.WriteLine("********Flip coin and print percentage**********");
+            Console.WriteLine("Enter No. of times you want to flip the coin");
+            int NumofCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < NumofCount; i++)
+            {
+                Random randombj = new Random();
+                int randomheadtail = randombj.Next(0, 1);
+                if (randomheadtail < 0.5)
+                {
+                    tailcount++;
+                }
+                else
+                {
+                    headcount++;
+                }
+                Console.WriteLine("------Percentage of Head vs Tail-------");
+                perOfHead = headcount / NumofCount * 100;
+                Console.WriteLine("Percentage of Head=" + perOfHead);
+                perOfTail = tailcount / NumofCount * 100;
+                Console.WriteLine("Percentage of Head=" + perOfTail);
+            }
+        }
+    }
+}
---
+++ BasicCore/Program.cs
@@ -0,0 +1,10 @@
+﻿using BasicCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FlipCoin flipCoin = new FlipCoin();
+        flipCoin.Main(args);
+    }
+}
